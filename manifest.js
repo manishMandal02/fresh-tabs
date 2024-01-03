fe60558ee0be3bb8709364e557ea8c3bbd0e9373
Manish Mandal <manishmandalj@gmail.com>
2024-01-03 20:00:23 +0530

    initial commit

---
+++ manifest.js
@@ -0,0 +1,33 @@
+/**
+ * After changing, please reload the extension at `chrome://extensions`
+ * @type {chrome.runtime.ManifestV3}
+ */
+const manifest = {
+  manifest_version: 3,
+  name: 'Fresh Tabs',
+  version: '0.0.1',
+  description: 'Manage tabs like pro',
+  permissions: ['storage', 'sidePanel'],
+  side_panel: {
+    default_path: 'src/pages/sidepanel/index.html',
+  },
+  options_page: 'src/pages/options/index.html',
+  background: {
+    service_worker: 'src/pages/background/index.js',
+    type: 'module',
+  },
+  action: {
+    default_icon: 'icon-34.png',
+  },
+  icons: {
+    128: 'icon-128.png',
+  },
+  web_accessible_resources: [
+    {
+      resources: ['assets/js/*.js', 'assets/css/*.css', 'icon-128.png', 'icon-34.png'],
+      matches: ['*://*/*'],
+    },
+  ],
+};
+
+export default manifest;
